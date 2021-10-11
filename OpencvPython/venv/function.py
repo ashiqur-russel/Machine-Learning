f4a6731583ed5fd9ddc65a868055e68ae5c8091f
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-11 03:30:41 +0200

    Detect edge of the photo using canny

--- OpencvPython/venv/function.py
+++ OpencvPython/venv/function.py
@@ -6,7 +6,10 @@ img = cv2.imread("1.jpeg")
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # convert gray image to blur image using GaussianBlur() function
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
+# find the edge of image using  Canny() function
+imgCanny = cv2.Canny(img,100,100)
 
 cv2.imshow("Gray Image", imgGray)
 cv2.imshow("Blur Image", imgBlur)
+cv2.imshow("Canny Image",imgCanny)
 cv2.waitKey(0)
